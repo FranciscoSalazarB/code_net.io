074c9afbf64c53e81cd76653e86ac97769980d24
Francisco Alonso Salazar Ballinas <franciscoalonso.salazarballinas@gmail.com>
2023-04-25 23:10:41 -0600

    (Dev) estructura base

---
+++ components/loginForm.tsx
@@ -0,0 +1,18 @@
+import "@/styles/module/loginForm.css"
+
+export function LoginForm() {
+    return (
+        <div>
+            <form>
+                <button className="neonBtn">
+                    <span />
+                    <span />
+                    <span />
+                    <span />
+                    Submit
+                </button>
+            </form>
+        </div>
+    )
+}
+
